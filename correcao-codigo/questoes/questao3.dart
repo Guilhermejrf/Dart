b9f93ac7befbc5bb57fa1a52d1a7bde2f00da362
Guilherme <164170606+Guilhermejrf@users.noreply.github.com>
2026-08-07 22:35:36 -0300

    Add files via upload

---
+++ correcao-codigo/questoes/questao3.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Minha aplicação')),
+        body: Center(child: Text('Flutter')),
+      ),
+    ),
+  );
+}
