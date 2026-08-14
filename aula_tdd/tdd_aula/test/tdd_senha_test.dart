9c0e937ece454d8ad47dd86c3724cfc73fc3e04c
Guilherme <164170606+Guilhermejrf@users.noreply.github.com>
2026-08-13 23:48:26 -0300

    Coloquei o resto

---
+++ aula_tdd/tdd_aula/test/tdd_senha_test.dart
@@ -0,0 +1,19 @@
+import 'package:test/test.dart';
+import 'package:.dart';
+
+void main() {
+    group('| Validador de Senha |', () {
+    test('Senha válida', () {
+      expect(() => validarSenha('Senha123'), returnsNormally);
+    });
+    test('Senha com menos de 8 caracteres', () {
+      expect(() => validarSenha('Senh1'), throwsArgumentError);
+    });
+    test('Senha sem letra maiúscula', () {
+      expect(() => validarSenha('senha123'), throwsArgumentError);
+    });
+    test('Senha sem número', () {
+      expect(() => validarSenha('SenhaSemNumero'), throwsArgumentError);
+    });
+  });
+}
