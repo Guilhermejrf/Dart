e11e82b9de1e723d7f040004d01afbe039a62c25
Guilherme <164170606+Guilhermejrf@users.noreply.github.com>
2026-08-13 23:47:03 -0300

    lembrei que estava com erros

--- aula_tdd/tdd_aula/lib/tdd_senha.dart
+++ aula_tdd/tdd_aula/lib/tdd_senha.dart
@@ -1,3 +1,11 @@
-String senha(String asenha){
-  
+void validarSenha(String senha) {
+  if (senha.length < 8) {
+    throw ArgumentError('A senha deve ter pelo menos 8 caracteres.');
+  }
+  if (!senha.contains(RegExp(r'[A-Z]'))) {
+    throw ArgumentError('A senha deve conter pelo menos uma letra maiúscula.');
+  }
+  if (!senha.contains(RegExp(r'[0-9]'))) {
+    throw ArgumentError('A senha deve conter pelo menos um número.');
+  }
 }
